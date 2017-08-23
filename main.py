10a5a922e91199ffbdc0ad6bcb3fc0a9c9dc8d87
Ivica Turic <ivica190793@gmail.com>
2017-08-23 21:09:46 +0200

    Reworked main into data_collect and added analog input
    Signed-off-by: Ivica Turic <ivica190793@gmail.com>

--- main.py
+++ main.py
@@ -1,76 +1,100 @@
 import numpy as np
 import cv2
-from time import sleep
-import win32api as wapi
 import time
-from inputCommands import DecideMove, UP, LEFT, RIGHT, ReleaseKey
-from getKeys import key_check
-from grabScreen import grab_screen
 import os
+import pygame
+import win32api as wapi
+from time import sleep
+from grabScreen import grab_screen
 from alexnet import alexnet
 
-def draw_lines(img,lines):
+WIDTH = 800
+HEIGHT = 360
+HEIGHT_OFFSET = 270
+
+# Initialize the joysticks
+pygame.init()
+pygame.joystick.init()
+
+
+def edit_img(image):    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image =  cv2.Canny(image, threshold1 = 200, threshold2=300)
+    frameResize=np.array([[250,360],[330,120],[470,120],[550,360]], np.int32)
+    cv2.fillPoly(image, [frameResize], 0)
+    image = cv2.GaussianBlur(image,(5,5),0)
+    lines = cv2.HoughLinesP(image, 0.9, np.pi/180, 80, 20, 10)
     if lines!=None:
         for line in lines:
             coords = line[0]
             cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 5)
+    return image
 
-def process_img(image):
-    original_image = image
-    
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # frameResize=np.array([[0,600],[0,230],[800,230],[800,600],[550,600],[470,360],[330,360],[250,600],
-    #                      ], np.int32)
-    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
-    frameResize=np.array([[250,360],[330,120],[470,120],[550,360]], np.int32)
-    # processed_img = roi(processed_img, [frameResize])
-    cv2.fillPoly(processed_img, [frameResize], 0)
-    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
-    lines = cv2.HoughLinesP(processed_img, 0.9, np.pi/180, 80, 20, 10)
-    # print(len(lines))
-    # sleep(0.5)
-    draw_lines(processed_img,lines)
-    return processed_img
 
-def main():
+def seperate_steering(input):
+    input_seperated=[0.0,0.0]
+    if input<0:
+        #left
+        input_seperated[0]=abs(input)
+    else:
+        #right
+        input_seperated[1]=input
+    return input_seperated
+
+
+def data_collect():
     last_time = time.time()
     fps=0
-    # for i in list(range(3)):
-    #     print(3-i)
-    #     time.sleep(1)
+    for i in list(range(3)):
+        print(3-i)
+        time.sleep(1)
 
-    # traning_file='shit.npy'
+    traning_file='data/shit.npy'
 
-    # if os.path.isfile(traning_file):
-    #     print('File exists, loading previous data!')
-    #     training_data = list(np.load(traning_file))
-    # else:
-    #     print('File does not exist, starting fresh!')
-    #     training_data = []
+    if os.path.isfile(traning_file):
+        print('Resuming old data file ', traning_file)
+        training_data = list(np.load(traning_file))
+    else:
+        print('Starting new data file ', traning_file)
+        training_data = []
     last_time = time.time()
+
+    joystick = pygame.joystick.Joystick(1)
+    joystick.init()
+    print("Recording data from joystick named: ",joystick.get_name())
 
     while True:
         fps=fps+1
-        screen =  grab_screen(region=(0,270,800,630))
-        screen = process_img(screen)
-        # screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-        screen=cv2.resize(screen, (80,36))
-        screen=cv2.resize(screen, (800,360))
+        sleep(0.2)
+
+        #get input from joystick
+        pygame.event.get()
+        steering_input=joystick.get_axis(0)
+        input_seperated=seperate_steering(steering_input)
+        print("LEFT: ", input_seperated[0], "  RIGHT: ", input_seperated[1]);
+
+        #get and edit screen image
+        screen =  grab_screen(region=(0,HEIGHT_OFFSET,WIDTH,HEIGHT+HEIGHT_OFFSET)) # It is 800x360 window
+        screen = edit_img(screen) #Comemnt this out for better FPS
+        screen=cv2.resize(screen, (WIDTH/10,HEIGHT/10))
         cv2.imshow('window', screen)
-        # screen=screen[5:34, 0:80]
-        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        
         if ((cv2.waitKey(25) & 0xFF == ord('q')) or wapi.GetAsyncKeyState(ord('Q'))):
             print(fps/(time.time()-last_time))
             cv2.destroyAllWindows()
             break
+
+        # if wapi.GetAsyncKeyState(ord('Q'))):
+        #     print(fps/(time.time()-last_time))
+        #     break
+
         # if len(training_data) % 250==0:
         #     print('Average FPS: ', fps/(time.time()-last_time))
         #     print('Traning data size: ',len(training_data))
         #     np.save(traning_file, training_data)
-        # training_data.append([screen,key_check()])
+        # training_data.append([screen,])
 
-WIDTH = 80
-HEIGHT = 29
+
 LR = 1e-3
 EPOCHS = 8
 MODEL_NAME = 'track-mania-self-driving-big.model'
@@ -114,7 +138,7 @@ def maintest():
 
 
 if __name__ == "__main__":
-    main()
+    data_collect()
 
 # if __name__ == '__main__':
 #     time.sleep(2)
